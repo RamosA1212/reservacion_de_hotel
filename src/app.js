5f87cac3c9fdb0af839b4005217e6252336307e5
RamosA1212 <127896904+RamosA1212@users.noreply.github.com>
2023-11-28 02:56:11 -0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 app.use(express.json())
 app.use(cors( ({
-    origin: "http://localhost:3000", // Reemplaza con el origen de tu frontend
+    origin: "https://reservar-front.onrender.com", 
     credentials: true,
 })))
 app.use(cookieParser())
